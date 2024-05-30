710676f2d3783e60e8676bb0851514b49d3272e5
Minoru Sakakibara <qqgm7z69@gmail.com>
2024-05-30 21:00:58 +0900

    1st commit

---
+++ monster.py
@@ -0,0 +1,24 @@
+import PySimpleGUI as sg
+import random
+sg.theme("DarkBrown3")
+
+layout = [[sg.T("あなたの名前を入力してください。")],
+          [sg.T("名前"),sg.I(k="input")],
+          [sg.B(" 実行 ", k="btn"), sg.T(k="txt")]]
+win = sg.Window("あなたをモンハンのモンスターに例えます", layout,
+                font=(None,14), size=(500,300))
+
+def monster():
+    monsters = ["リオレウス", "ジンオウガ", "ディノバルド", "ティガレックス",
+                "クシャルダオラ", "テオテスカトル", "バルハザク", "ネルギガンテ",
+                "イヴェルカーナ"]
+    name = random.choice(monsters)
+    win["txt"].update(name)
+
+while True:
+    e, v = win.read()
+    if e == "btn":
+      monster()
+    if e == None:
+        break
+win.close()
